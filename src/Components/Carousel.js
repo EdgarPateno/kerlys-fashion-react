bbf1879553012e6a467c8cec2f378e59de59ee57
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-08 21:16:39 +0800

    FeaturedProductsCarousel finalized

---
+++ src/Components/Carousel.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Carousel = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4, // Number of cards to show at a time
+        slidesToScroll: 1, // Number of cards to slide on each click
+        autoplay: true, // Automatically slide the carousel
+        autoplaySpeed: 2000, // Delay between each slide in milliseconds
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <h2>Carousel Example</h2>
+            <Slider {...settings}>
+                <div>
+                    <div className="card">
+                        <img src="./images/featured-products-carousel/1.webp" class="card-img-top" alt="Featured Product 1" />
+                        <h3>Card 1</h3>
+                        <p>Card 1 Content</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="card">
+                        <img src="./images/featured-products-carousel/2.webp" class="card-img-top" alt="Featured Product 2" />
+                        <h3>Card 2</h3>
+                        <p>Card 2 Content</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="card">
+                        <img src="./images/featured-products-carousel/3.webp" class="card-img-top" alt="Featured Product 3" />
+                        <h3>Card 3</h3>
+                        <p>Card 3 Content</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="card">
+                        <img src="./images/featured-products-carousel/4.webp" class="card-img-top" alt="Featured Product 4" />
+                        <h3>Card 4</h3>
+                        <p>Card 4 Content</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="card">
+                        <img src="./images/featured-products-carousel/5.webp" class="card-img-top" alt="Featured Product 5" />
+                        <h3>Card 5</h3>
+                        <p>Card 5 Content</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="card">
+                        <img src="./images/featured-products-carousel/6.webp" class="card-img-top" alt="Featured Product 6" />
+                        <h3>Card 6</h3>
+                        <p>Card 6 Content</p>
+                    </div>
+                </div>
+            </Slider>
+        </div>
+    );
+};
+
+export default Carousel;
--- src/Components/FeaturedProductsCarousel.js
+++ src/Components/FeaturedProductsCarousel.js
@@ -1,54 +1,105 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const FeaturedProductsCarousel = () => {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4, // Number of cards to show at a time
+        slidesToScroll: 1, // Number of cards to slide on each click
+        autoplay: true, // Automatically slide the carousel
+        autoplaySpeed: 3000, // Delay between each slide in milliseconds
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+        ],
+    };
+
     return (
-        <section className='featured-products-carousel my-5'>
+        <section className="featured-products-carousel my-5">
             <div className='container'>
-                <h2 className='h1 mb-4 text-center'>OUR BEST SELLERS</h2>
-                <Carousel prevIcon={<BsChevronLeft />} nextIcon={<BsChevronRight />}>
-                    <Carousel.Item style={{ backgroundColor: 'white' }}>
-                        <div className="d-flex justify-content-between gap-3">
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/1.webp" className="card-img-top" alt="Bag 1" />
-                            </div>
-
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/2.webp" className="card-img-top" alt="Jewelry 1" />
-                            </div>
-
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/3.webp" className="card-img-top" alt="Shoes 1" />
-                            </div>
-
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/4.webp" className="card-img-top" alt="Jewelry 2" />
+                <h2 className='featured-products-carousel-title text-center h1'>OUR BEST SELLERS</h2>
+                <Slider {...settings}>
+                    <div className='featured-product-content-wrapper'>
+                        <div className="card">
+                            <img src="./images/featured-products-carousel/1.webp" class="card-img-top" alt="Featured Product 1" />
+                            <div className="featured-product-text-content text-center">
+                                <h3>Daenarys</h3>
+                                <p>
+                                    <span className='featured-product regular-price'>$700 </span>
+                                    <span className='featured-product sale-price'>$600</span>
+                                </p>
                             </div>
                         </div>
-                    </Carousel.Item>
-
-                    <Carousel.Item style={{ backgroundColor: 'white' }}>
-                        <div className="d-flex justify-content-between gap-3">
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/5.webp" className="card-img-top" alt="Shoes 2" />
+                    </div>
+                    <div className='featured-product-content-wrapper'>
+                        <div className="card">
+                            <img src="./images/featured-products-carousel/2.webp" class="card-img-top" alt="Featured Product 2" />
+                            <div className="featured-product-text-content text-center">
+                                <h3>Greyjoy</h3>
+                                <p>
+                                    <span className='featured-product regular-price'>$800 </span>
+                                    <span className='featured-product sale-price'>$700</span>
+                                </p>
                             </div>
-
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/6.webp" className="card-img-top" alt="Jewelry 3" />
-                            </div>
-
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/7.webp" className="card-img-top" alt="Shoes 3" />
-                            </div>
-
-                            <div className="card">
-                                <img src="./images/featured-products-carousel/8.webp" className="card-img-top" alt="Shoes 4" />
-                            </div>
-
                         </div>
-                    </Carousel.Item>
-                </Carousel>
+                    </div>
+                    <div className='featured-product-content-wrapper'>
+                        <div className="card">
+                            <img src="./images/featured-products-carousel/3.webp" class="card-img-top" alt="Featured Product 3" />
+                            <div className="featured-product-text-content text-center">
+                                <h3>Aeliana</h3>
+                                <p>
+                                    <span className='featured-product regular-price'>$900 </span>
+                                    <span className='featured-product sale-price'>$800</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='featured-product-content-wrapper'>
+                        <div className="card">
+                            <img src="./images/featured-products-carousel/4.webp" class="card-img-top" alt="Featured Product 4" />
+                            <div className="featured-product-text-content text-center">
+                                <h3>Tyene</h3>
+                                <p>
+                                    <span className='featured-product regular-price'>$700 </span>
+                                    <span className='featured-product sale-price'>$600</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='featured-product-content-wrapper'>
+                        <div className="card">
+                            <img src="./images/featured-products-carousel/5.webp" class="card-img-top" alt="Featured Product 5" />
+                            <div className="featured-product-text-content text-center">
+                                <h3>Gilly</h3>
+                                <p>
+                                    <span className='featured-product regular-price'>$900 </span>
+                                    <span className='featured-product sale-price'>$800</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='featured-product-content-wrapper'>
+                        <div className="card">
+                            <img src="./images/featured-products-carousel/6.webp" class="card-img-top" alt="Featured Product 6" />
+                            <div className="featured-product-text-content text-center">
+                                <h3>Idylle </h3>
+                                <p>
+                                    <span className='featured-product regular-price'>$900 </span>
+                                    <span className='featured-product sale-price'>$800</span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </Slider>
             </div>
         </section>
     );
