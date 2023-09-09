64cae4c5cda34239840dccc44c5bbf8d03a3ecb7
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-09 16:13:50 +0800

    we already used Header.js, so removed AnnouncementBanner and MainNav components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import CookieConsent from './Components/CookieConsent';
 import TawkTo from './Components/Tawkto';
-// import AnnouncementBanner from './Components/AnnouncementBanner';
-// import MainNav from './Components/MainNav';
 import Header from './Components/Header';
 import HomepageBannerBag from './Components/HomepageVideoBannerBag';
 import ShopByBrands from './Components/ShopByBrands';
@@ -20,8 +18,6 @@ function App() {
     <div>
       <CookieConsent />
       <TawkTo />
-      {/* <AnnouncementBanner />
-      <MainNav /> */}
       <Header />
       <HomepageBannerBag />
       <FeaturedProductsCarousel />
