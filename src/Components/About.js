f2a6f96a68129a7e5b6766c9e141f3b3234e3554
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-11 19:48:52 +0800

    created About component

---
+++ src/Components/About.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function About() {
+    return (
+        <section class="about-us-page">
+            <div class="container-sm my-5">
+                <h2 class="about-us-heading text-center mb-5">ABOUT US</h2>
+                <p>Welcome to KERLYS FASHION, your #1 source of premium quality luxury bags, shoes, jewellery, etc.</p>
+
+                <p>We're dedicated to giving you the very best of fashion items - with a focus on QUALITY, AFFORDABILITY,
+                    DURABILITY, and CUSTOMER SERVICE.</p>
+
+                <p>Founded in 2022 by fashion experts, KERLYS FASHION has come a long way from its humble beginnings. When
+                    the team first started out, their passion for "excellent quality luxury fashion items" drove them to
+                    do tons of research so that they can offer you long-lasting fashion products.</p>
+
+                <p>We now serve customers all over the US, and are thrilled that we're able to turn our passion into a REALITY.
+                </p>
+
+                <p>We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions or comments,
+                    please don't hesitate to contact us.</p>
+
+                <p class="mb-4">Sincerely,</p>
+
+                <p>KERLYS FASHION Team</p>
+            </div>
+        </section>
+    )
+}
+
+export default About
