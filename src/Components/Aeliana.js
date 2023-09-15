772eb1cc81746cd0aa4fa0a5cda2b1f8525c07d9
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-15 21:04:03 +0800

    converted Aeliana product tabs reusable

--- src/Components/Aeliana.js
+++ src/Components/Aeliana.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../CSS/Aeliana.css'
+import ProductTabs from './ProductTabs';
 
 const Aeliana = () => {
 
@@ -174,79 +175,27 @@ const Aeliana = () => {
                     </div>
                 </div>
             </section>
-
-            <section className='container'>
-                <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                        <button
-                            className={`nav-link ${activeTab === 'description' ? 'active' : ''}`}
-                            onClick={() => setActiveTab('description')}
-                        >
-                            DESCRIPTION
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            className={`nav-link ${activeTab === 'shipping' ? 'active' : ''}`}
-                            onClick={() => setActiveTab('shipping')}
-                        >
-                            SHIPPING
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            className={`nav-link ${activeTab === 'return' ? 'active' : ''}`}
-                            onClick={() => setActiveTab('return')}
-                        >
-                            RETURN
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button
-                            className={`nav-link ${activeTab === 'guarantee' ? 'active' : ''}`}
-                            onClick={() => setActiveTab('guarantee')}
-                        >
-                            GUARANTEE
-                        </button>
-                    </li>
-                </ul>
-
+            <section className="container">
+                <ProductTabs activeTab={activeTab} setActiveTab={setActiveTab} />
                 <div className="tab-content">
                     <div className={`tab-pane ${activeTab === 'description' ? 'active' : ''}`}>
-                        <p className='mt-4'>
-                            Introducing the exquisite Aeliana Luxury Bag – a true embodiment of elegance and sophistication. Crafted with the utmost precision and attention to detail, this masterpiece redefines luxury in every sense.</p>
+                        <p className="mt-4">
+                            Introducing the exquisite Aeliana Luxury Bag – a true embodiment of elegance and sophistication. Crafted
+                            with the utmost precision and attention to detail, this masterpiece redefines luxury in every sense.</p>
 
-                        <p>Enveloped in the gentle embrace of a soothing baby blue hue, the Aeliana Luxury Bag exudes an aura of timeless charm and refined beauty. The color evokes a sense of tranquility and grace, making it the perfect companion for any occasion, whether it be a casual outing or a glamorous soirée.</p>
+                        <p>Enveloped in the gentle embrace of a soothing baby blue hue, the Aeliana Luxury Bag exudes an aura of
+                            timeless charm and refined beauty. The color evokes a sense of tranquility and grace, making it the
+                            perfect companion for any occasion, whether it be a casual outing or a glamorous soirée.</p>
 
-                        <p>Indulge in the opulence of genuine leather as your fingertips graze across the impeccably smooth surface of the Aeliana bag. This carefully selected leather not only enhances the bag's durability but also lends an air of exclusivity, ensuring that your accessory stands the test of time while becoming a symbol of your impeccable taste.</p>
-                    </div>
-
-                    <div className={`tab-pane ${activeTab === 'shipping' ? 'active' : ''}`}>
-                        <p className="mt-4">When you place your order, we know that you want to receive the item as fast as possible. We will make that happen.</p>
-                        <p>How long will the entire ordering process take from order submission to delivery?</p>
-                        <p>We will do our best to deliver your product as fast as possible, depending on your location.</p>
-                        <p className="display-6">USA only:</p>
-                        <ul>
-                            <li>Standard Shipping: 5-10 Business Days via USPS (expect a slight delay due to COVID)</li>
-                            <li>Express Shipping: 3-5 Business Days via FedEx </li>
-                        </ul>
-                    </div>
-
-                    <div className={`tab-pane ${activeTab === 'return' ? 'active' : ''}`}>
-                        <p className="mt-4">We have a pretty straightforward and simple return, refund, and exchange policy.</p>
-                        <p>When you receive your order and want to exchange it with a different product, it MUST be unopened, unused, in original packaging, and with all the labels. First, you will need to contact us stating your reason for exchange. Then, you will need to ship the original order back to us, at your own expense. Once we receive it, we will inspect if it's unopened, unused, in original packaging, and with all the labels. Then, we will issue a store credit in the form of a code, and you will use that code to make a new purchase.</p>
-                        <p>When you receive your order and have changed your mind and you want to return it, it MUST be unopened, unused, in original packaging, and with all the labels. First, you will need to contact us stating your reason for returning the item/s. Then, you will need to ship the original order back to us, at your own expense. Once we receive it, we will inspect if it's unopened, unused, in original packaging, and with all the labels. Then, we will issue a store credit only in the form of a code, not a full refund.</p>
-                    </div>
-
-                    <div className={`tab-pane ${activeTab === 'guarantee' ? 'active' : ''}`}>
-                        <p className="mt-4">Our Guarantee of Uncompromising Excellence:</p>
-                        <p>At KERLYS FASHION, we take great pride in the craftsmanship and quality of our luxury shoes. We stand firmly behind the excellence of our products and offer a comprehensive guarantee to ensure your absolute satisfaction.
-                        </p>
+                        <p>Indulge in the opulence of genuine leather as your fingertips graze across the impeccably smooth surface
+                            of the Aeliana bag. This carefully selected leather not only enhances the bag's durability but also lends
+                            an air of exclusivity, ensuring that your accessory stands the test of time while becoming a symbol of
+                            your impeccable taste.</p>
                     </div>
                 </div>
             </section>
-
         </div>
+
     )
 }
 
---
+++ src/Components/ProductTabs.js
@@ -0,0 +1,67 @@
+// ProductTabs.js
+
+import React from 'react';
+
+const ProductTabs = ({ activeTab, setActiveTab }) => {
+    return (
+        <div>
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <button
+                        className={`nav-link ${activeTab === 'description' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('description')}
+                    >
+                        DESCRIPTION
+                    </button>
+                </li>
+                <li className="nav-item">
+                    <button
+                        className={`nav-link ${activeTab === 'shipping' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('shipping')}
+                    >
+                        SHIPPING
+                    </button>
+                </li>
+                <li className="nav-item">
+                    <button
+                        className={`nav-link ${activeTab === 'return' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('return')}
+                    >
+                        RETURN
+                    </button>
+                </li>
+                <li className="nav-item">
+                    <button
+                        className={`nav-link ${activeTab === 'guarantee' ? 'active' : ''}`}
+                        onClick={() => setActiveTab('guarantee')}
+                    >
+                        GUARANTEE
+                    </button>
+                </li>
+            </ul>
+
+            <div className="tab-content">
+
+                <div className={`tab-pane ${activeTab === 'shipping' ? 'active' : ''}`}>
+                    <p className="mt-4">When you place your order, we know that you want to receive the item as fast as possible. We will make that happen.</p>
+                    <p>How long will the entire ordering process take from order submission to delivery?</p>
+                    <p>We will do our best to deliver your product as fast as possible, depending on your location.</p>
+                    <p className="display-6">USA only:</p>
+                    <ul>
+                        <li>Standard Shipping: 5-10 Business Days via USPS (expect a slight delay due to COVID)</li>
+                        <li>Express Shipping: 3-5 Business Days via FedEx </li>
+                    </ul>
+                </div>
+
+                <div className={`tab-pane ${activeTab === 'return' ? 'active' : ''}`}>
+                    <p className="mt-4">We have a pretty straightforward and simple return, refund, and exchange policy.</p>
+                    <p>When you receive your order and want to exchange it with a different product, it MUST be unopened, unused, in original packaging, and with all the labels. First, you will need to contact us stating your reason for exchange. Then, you will need to ship the original order back to us, at your own expense. Once we receive it, we will inspect if it's unopened, unused, in original packaging, and with all the labels. Then, we will issue a store credit in the form of a code, and you will use that code to make a new purchase.</p>
+                    <p>When you receive your order and have changed your mind and you want to return it, it MUST be unopened, unused, in original packaging, and with all the labels. First, you will need to contact us stating your reason for returning the item/s. Then, you will need to ship the original order back to us, at your own expense. Once we receive it, we will inspect if it's unopened, unused, in original packaging, and with all the labels. Then, we will issue a store credit only in the </p>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default ProductTabs;
