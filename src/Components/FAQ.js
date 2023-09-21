6097fb7a4590e1a76948daef00d52ff00d81a9cb
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-22 00:55:05 +0800

    added one question to faq

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -5,7 +5,7 @@ const FAQ = () => {
     return (
         <div className="faq container">
             <h2 className="text-center h1 my-5">Frequently Asked Questions</h2>
-            <Accordion defaultActiveKey="0">
+            <Accordion>
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>What shipping options are available?</Accordion.Header>
                     <Accordion.Body>
@@ -20,6 +20,12 @@ const FAQ = () => {
                     <Accordion.Header>How can I track my order?</Accordion.Header>
                     <Accordion.Body>
                         <p>A shipping notification with tracking number will be sent via email after your order has been shipped out. You may click on that tracking number to get the latest available update. You can also visit the website and click on Track Order tab on the header and footer sections.</p>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>Do you ship internationally?</Accordion.Header>
+                    <Accordion.Body>
+                        <p>Yes, we do ship worldwide.  Shipping costs will depend on your location unless otherwise stated as free shipping.</p>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
