0da2577f5f0ef6cedbdc60ffaa3622ebf60fe128
Edgar Pateño <edgarpateno80@gmail.com>
2023-09-26 23:35:49 +0800

    added <small> element below <h4> on BelowATCBadges component

--- src/Components/BelowATCBadges.js
+++ src/Components/BelowATCBadges.js
@@ -8,14 +8,17 @@ const BelowATCBadges = () => {
                 <div className="shipping col text-center">
                     <img src="/logos-icons/belowATC-badges/free-shipping.webp" alt="" />
                     <h4>Free Shipping</h4>
+                    <small>From our US Warehouse</small>
                 </div>
                 <div className="happiness col text-center">
                     <img src="/logos-icons/belowATC-badges/money-back.webp" alt="" />
                     <h4>30 Day</h4>
+                    <small>Happiness Guarantee</small>
                 </div>
                 <div className="warranty col text-center">
                     <img src="/logos-icons/belowATC-badges/warranty.webp" alt="" />
                     <h4>One Year</h4>
+                    <small>Replacement Warranty</small>
                 </div>
             </div>
         </div>
