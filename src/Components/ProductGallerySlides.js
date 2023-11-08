5491239a4e9f0a7320c7c64d11790836827006a3
Edgar Pateño <edgarpateno80@gmail.com>
2023-11-08 19:26:09 +0800

    added showSlides on line 27

--- src/Components/ProductGallerySlides.js
+++ src/Components/ProductGallerySlides.js
@@ -1,60 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import '../CSS/ProductGallerySlides.css';
+import React, { useState, useEffect } from "react";
+import "../CSS/ProductGallerySlides.css";
 
 const ProductGallerySlides = ({ images }) => {
-    const [slideIndex, setSlideIndex] = useState(1);
+  const [slideIndex, setSlideIndex] = useState(1);
 
-    const showSlides = (n) => {
-        if (n < 1) {
-            setSlideIndex(images.length);
-        } else if (n > images.length) {
-            setSlideIndex(1);
-        } else {
-            setSlideIndex(n);
-        }
-    };
+  const showSlides = (n) => {
+    if (n < 1) {
+      setSlideIndex(images.length);
+    } else if (n > images.length) {
+      setSlideIndex(1);
+    } else {
+      setSlideIndex(n);
+    }
+  };
 
-    const plusSlides = (n) => {
-        showSlides(slideIndex + n);
-    };
+  const plusSlides = (n) => {
+    showSlides(slideIndex + n);
+  };
 
-    const currentSlide = (n) => {
-        showSlides(n);
-    };
+  const currentSlide = (n) => {
+    showSlides(n);
+  };
 
-    useEffect(() => {
-        showSlides(slideIndex);
-    }, [slideIndex]);
+  useEffect(() => {
+    showSlides(slideIndex);
+  }, [slideIndex, showSlides]);
 
-    return (
-        <div className="product-gallery container">
-            <div className="col">
-                <div className="slides">
-                    {images.map((image, index) => (
-                        <div
-                            key={index}
-                            className={`slide ${index + 1 === slideIndex ? 'active' : ''}`}
-                        >
-                            <img src={image} alt={`Product Image ${index + 1}`} />
-                        </div>
-                    ))}
-                </div>
-                <a className="prev" onClick={() => plusSlides(-1)}>❮</a>
-                <a className="next" onClick={() => plusSlides(1)}>❯</a>
-                <div className="thumbnails">
-                    {images.map((image, index) => (
-                        <div
-                            key={index}
-                            className={`thumbnail cursor ${index + 1 === slideIndex ? 'active' : ''}`}
-                            onClick={() => currentSlide(index + 1)}
-                        >
-                            <img src={image} alt={`Thumbnail ${index + 1}`} />
-                        </div>
-                    ))}
-                </div>
+  return (
+    <div className="product-gallery container">
+      <div className="col">
+        <div className="slides">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`slide ${index + 1 === slideIndex ? "active" : ""}`}
+            >
+              <img src={image} alt={`Product Image ${index + 1}`} />
             </div>
+          ))}
         </div>
-    );
+        <a className="prev" onClick={() => plusSlides(-1)}>
+          ❮
+        </a>
+        <a className="next" onClick={() => plusSlides(1)}>
+          ❯
+        </a>
+        <div className="thumbnails">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className={`thumbnail cursor ${
+                index + 1 === slideIndex ? "active" : ""
+              }`}
+              onClick={() => currentSlide(index + 1)}
+            >
+              <img src={image} alt={`Thumbnail ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductGallerySlides;
